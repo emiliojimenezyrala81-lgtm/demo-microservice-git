64d7a6115b434d06cc91e2ed98f249dca6746ea4
EMILIO575 <emiliojimenezyrala41@gmail.com>
2025-10-31 05:16:54 -0400

    Usar server.js para iniciar y arreglar Dockerfile

--- app/app.js
+++ app/app.js
@@ -3,9 +3,9 @@ import express from "express";
 const app = express();
 
 
-//app.get("/", (_req, res) => {
-  //  res.json({ status: "ok", service: "Hola soy el BARTO2" });
-//});
+app.get("/", (_req, res) => {
+    res.json({ status: "ok", service: "Hola soy el BARTO2" });
+});
 
 
 app.get("/api/usuarios", (_req, res) => {
@@ -14,7 +14,27 @@ app.get("/api/usuarios", (_req, res) => {
         { id: 2, nombre: "Emilio" },
         { id: 3, nombre: "Carlos" }
     ];
-    res.json(usuarios); 
+    res.json(usuarios);
+});
+
+
+app.get("/api/productos", (_req, res) => {
+    const productos = [
+        { id: 100, nombre: "Laptop", stock: 15 },
+        { id: 101, nombre: "Teclado", stock: 30 }
+    ];
+    res.json(productos);
+});
+
+
+app.get("/api/usuarios/:id", (req, res) => {
+    const idBuscado = req.params.id; 
+
+    
+    res.json({ 
+        buscando_usuario_con_id: idBuscado,
+        nombre: "Usuario de Ejemplo"
+    });
 });
 
 
