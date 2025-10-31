667ca428f4572ab922744bd874647355b76ae0ce
EMILIO575 <emiliojimenezyrala41@gmail.com>
2025-10-31 17:19:13 -0400

    Forzar nueva 23

--- app/app.js
+++ app/app.js
@@ -42,7 +42,7 @@ app.get("/api/resumen", (_req, res) => {
     // 1. Define o importa los datos que ya tienes
     const usuarios = [
         { id: 1, nombre: "Ana" },
-        { id: 2, nombre: "Emilio" },
+        { id: 2, nombre: "Emi" },
         { id: 3, nombre: "Carlos" }
     ];
     const productos = [
