6c123989ab4d97e92cb2e59fcd62b1fd7d18fc12
EMILIO575 <emiliojimenezyrala41@gmail.com>
2025-10-31 04:22:46 -0400

    cambio en get.js

--- app/server.js
+++ app/server.js
@@ -1,8 +1,10 @@
 import app from "./app.js";
 
 const PORT = process.env.PORT || 3000;
-const server = app.listen(PORT, () => {
-  //
+const HOST = '0.0.0.0'; // <--- AÑADE ESTA LÍNEA
+
+const server = app.listen(PORT, HOST, () => { // <--- AÑADE EL HOST AQUÍ
+    console.log(`Servidor corriendo en http://${HOST}:${PORT}`); // (Añade esto para saber que funcionó)
 });
 
 process.on("SIGTERM", () => server.close(() => process.exit(0)));
